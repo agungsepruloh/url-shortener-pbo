260c6afdc9ab8d8f9558051a6e29c06c82822780
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-01-06 06:16:45 +0700

    Login page design

---
+++ src/main/java/com/pbo/urlshortener/SecurityConfiguration.java
@@ -0,0 +1,46 @@
+package com.pbo.urlshortener;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                // URLs matching for access rights
+                .antMatchers("/").permitAll()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/register").permitAll()
+                .antMatchers("/dashboard/**").hasAnyAuthority("SUPER_USER", "ADMIN_USER", "SITE_USER")
+                .anyRequest().authenticated()
+                .and()
+                // form login
+                .csrf().disable().formLogin()
+                .loginPage("/login")
+                .failureUrl("/login?error=true")
+                .defaultSuccessUrl("/home")
+                .usernameParameter("email")
+                .passwordParameter("password")
+                .and()
+                // logout
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/").and()
+                .exceptionHandling()
+                .accessDeniedPage("/access-denied");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+    }
+
+}
---
+++ src/main/java/com/pbo/urlshortener/controllers/LoginController.java
@@ -0,0 +1,18 @@
+package com.pbo.urlshortener.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(value = { "/login" }, method = RequestMethod.GET)
+    public ModelAndView login() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("pages/login"); // resources/templates/login.html
+        return modelAndView;
+    }
+
+}
