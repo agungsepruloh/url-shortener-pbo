fd0184b5f0e29e377b8d61f13876c21327e09b7e
Agung Sepruloh <agungsepruloh1996@gmail.com>
2020-01-15 17:07:17 +0700

    Add new URL modal

--- src/main/java/com/pbo/urlshortener/controllers/LoginController.java
+++ src/main/java/com/pbo/urlshortener/controllers/LoginController.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class LoginController {
 
-    @RequestMapping(value = { "/login" }, method = RequestMethod.GET)
+    @RequestMapping(value = {"/", "/login" }, method = RequestMethod.GET)
     public ModelAndView login() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("pages/login"); // resources/templates/pages/login.html
