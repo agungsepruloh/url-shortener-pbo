5d5685f0a8605b4ac88f91ae90f5418722d2f9d8
fyzd21 <fadlullahyazid@gmail.com>
2020-02-05 14:23:13 +0700

    Add edit profile function

--- src/main/java/com/pbo/urlshortener/controllers/ProfileController.java
+++ src/main/java/com/pbo/urlshortener/controllers/ProfileController.java
@@ -2,14 +2,19 @@ package com.pbo.urlshortener.controllers;
 
 import com.pbo.urlshortener.models.User;
 import com.pbo.urlshortener.repositories.UserRepository;
+import com.pbo.urlshortener.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
 public class ProfileController {
@@ -17,14 +22,30 @@ public class ProfileController {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    UserService userService;
+
     @RequestMapping(value = {"/profile"}, method = RequestMethod.GET)
-    public ModelAndView profile(HttpServletRequest request, Model model) {
+    public ModelAndView profile(HttpServletRequest request,  Model model) {
         String email = request.getUserPrincipal().getName();
         User user = userRepository.findByEmail(email);
         model.addAttribute("user", user);
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("pages/profile"); // resources/templates/pages/profile.html
         return modelAndView;
+    }
+
+
+    @RequestMapping(value = {"/profile/update"}, method = RequestMethod.POST)
+    public ModelAndView update_profile(@Valid User user, BindingResult bindingResult, ModelMap modelMap) {
+        ModelAndView modelAndView = new ModelAndView();
+
+            userService.updateUser(user);
+            modelAndView.addObject("successMessage", "User successfully updated!");
+        modelAndView.addObject("user", new User());
+        modelAndView.setViewName("pages/profile");
+
+        return new ModelAndView("redirect:/profile");
     }
 
 }
--- src/main/java/com/pbo/urlshortener/services/UserServiceImp.java
+++ src/main/java/com/pbo/urlshortener/services/UserServiceImp.java
@@ -1,15 +1,19 @@
 package com.pbo.urlshortener.services;
 
 import com.pbo.urlshortener.models.Role;
+import com.pbo.urlshortener.models.Url;
 import com.pbo.urlshortener.models.User;
 import com.pbo.urlshortener.repositories.RoleRepository;
 import com.pbo.urlshortener.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 @Service
 public class UserServiceImp implements UserService {
@@ -31,8 +35,16 @@ public class UserServiceImp implements UserService {
     }
 
     @Override
-    public void updateUser(User user) {
+    public void updateUser(User newUser) {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByEmail(email);
 
+        user.setFirstName(newUser.getFirstName());
+        user.setLastName(newUser.getLastName());
+        user.setEmail(newUser.getEmail());
+        user.setPassword(encoder.encode(newUser.getPassword()));
+
+        userRepository.save(user);
     }
 
     @Override
